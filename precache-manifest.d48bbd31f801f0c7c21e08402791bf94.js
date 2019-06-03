aa778e0377870fface75a07ba6a4faa4981722f1
BonnieYu <Bonnie_Yu@wistron.com>
2019-06-04 01:09:37 +0800

    Updates

---
+++ precache-manifest.d48bbd31f801f0c7c21e08402791bf94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c97037112e781535b28ba3b807e4cb",
+    "url": "/hoc-sample1/index.html"
+  },
+  {
+    "revision": "394f04eae2bdc23efb1a",
+    "url": "/hoc-sample1/static/css/main.7132b8b0.chunk.css"
+  },
+  {
+    "revision": "80a00d4ccba5f136e61e",
+    "url": "/hoc-sample1/static/js/2.4d699d4e.chunk.js"
+  },
+  {
+    "revision": "394f04eae2bdc23efb1a",
+    "url": "/hoc-sample1/static/js/main.21db0741.chunk.js"
+  },
+  {
+    "revision": "f59adb3e767fda1001c6",
+    "url": "/hoc-sample1/static/js/runtime~main.53f4f327.js"
+  }
+]);
